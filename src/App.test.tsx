c0b82b0eb9596c7d6821a1bcc392f013d0750331
Iain E. Davis <iain@iaindavis.name>
2020-05-03 20:28:34 -0600

    Add the provider to App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import App from './App'
 import {shallow} from 'enzyme'
+import {PersonContextProvider} from './PersonContext/PersonContext'
 
 describe('App', () => {
     it('renders', () => {
         const app = shallow(<App/>)
 
         expect(app).not.toBeEmptyRender()
+    })
+
+    it('has the PersonContextProvider', () => {
+        const app = shallow(<App/>)
+
+        expect(app.find(PersonContextProvider)).toHaveLength(1)
     })
 })
