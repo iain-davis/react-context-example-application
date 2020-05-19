f9df5476552f88d4e0a34004ce3186d403724afc
Iain E. Davis <iain@iaindavis.name>
2020-05-19 08:37:17 -0600

    Default state for easier expansion

--- src/PersonContext/PersonContext.tsx
+++ src/PersonContext/PersonContext.tsx
@@ -4,7 +4,8 @@ export type Person = {
     name: string
 }
 
-export const PersonContext = React.createContext({name: ''})
+const defaultPerson = {name: ''}
+export const PersonContext = React.createContext(defaultPerson)
 
 export function PersonContextProvider(properties: { initialPerson: Person, children?: ReactNode }) {
     return <PersonContext.Provider value={properties.initialPerson}>{properties.children}</PersonContext.Provider>
