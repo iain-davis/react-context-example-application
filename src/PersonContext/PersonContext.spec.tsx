50565ccfadfeae356496aab25aeebce0e7ebf2ff
Iain E. Davis <iain@iaindavis.name>
2020-05-19 20:10:43 -0600

    Implement the state change provider

--- src/PersonContext/PersonContext.spec.tsx
+++ src/PersonContext/PersonContext.spec.tsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 import {mount} from 'enzyme'
-import {Person, PersonContext, PersonContextProvider} from './PersonContext'
+import {Person, PersonContext, PersonContextProvider, PersonStateChangeContext} from './PersonContext'
 
 describe('PersonContextProvider', () => {
     it('does not add to the rendering', () => {
@@ -25,9 +25,30 @@ describe('PersonContextProvider', () => {
         expect(provider.find('p').text()).toEqual('name')
     })
 
+    it('provides the ability to change the person', () => {
+        const newPerson: Person = {name: 'some new person name'}
+
+        let provider = mount(
+            <PersonContextProvider>
+                <TestPersonConsumingComponent/>
+                <TestComponentThatChangesPersonContext person={newPerson}/>
+            </PersonContextProvider>
+        )
+
+        provider.find('button').simulate('click')
+
+        expect(provider.find('p').text()).toEqual('some new person name')
+    })
+
     function TestPersonConsumingComponent() {
         const person: Person = useContext(PersonContext)
 
         return <p>{person.name}</p>
+    }
+
+    function TestComponentThatChangesPersonContext(properties: { person: Person }) {
+        const setPerson = useContext(PersonStateChangeContext)
+
+        return <button onClick={() => setPerson(properties.person)}>Click Me</button>
     }
 })
--- src/PersonContext/PersonContext.tsx
+++ src/PersonContext/PersonContext.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react'
+import React, {ReactNode, useState} from 'react'
 
 export type Person = {
     name: string
@@ -7,8 +7,17 @@ export type Person = {
 const defaultPerson: Person = {name: ''}
 export const PersonContext = React.createContext(defaultPerson)
 
+const defaultPersonStateChange: React.Dispatch<React.SetStateAction<Person>> = (personStateChange: ((prevState: Person) => Person) | Person) => {}
+export const PersonStateChangeContext = React.createContext(defaultPersonStateChange)
+
 export function PersonContextProvider(properties: { initialPerson: Person, children?: ReactNode }) {
-    return <PersonContext.Provider value={properties.initialPerson}>{properties.children}</PersonContext.Provider>
+    const [person, setPerson]: [Person, React.Dispatch<React.SetStateAction<Person>>] = useState(properties.initialPerson)
+
+    return <PersonContext.Provider value={person}>
+        <PersonStateChangeContext.Provider value={setPerson}>
+            {properties.children}
+        </PersonStateChangeContext.Provider>
+    </PersonContext.Provider>
 }
 
 PersonContextProvider.defaultProps = {
